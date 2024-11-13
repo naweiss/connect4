e850a25039e9fc8281b2f70acb01b244d8197908
dvir-k <dvirkerner@gmail.com>
2024-11-13 14:19:48 +0200

    Evaluate score also for the opponent, and improvement in efficiency

--- evaluation.py
+++ evaluation.py
@@ -27,6 +27,8 @@ class GreedyEvaluator:
             for player, pieces in itertools.groupby(game.board[row, :]):
                 if player == evaluated_player:
                     score += cls.LENGTH_TO_SCORE[len(list(pieces))]
+                    if score == math.inf:
+                        return score
         return score
 
     @classmethod
@@ -36,6 +38,8 @@ class GreedyEvaluator:
             for player, pieces in itertools.groupby(game.board[:, column]):
                 if player == evaluated_player:
                     score += cls.LENGTH_TO_SCORE[len(list(pieces))]
+                    if score == math.inf:
+                        return score
         return score
 
     @classmethod
@@ -43,18 +47,22 @@ class GreedyEvaluator:
         score = 0.0
         for diagonal_offset in range(-Connect4Game.BOARD_SIZE[1] + 1, Connect4Game.BOARD_SIZE[1]):
             diagonal = np.diagonal(game.board, offset=diagonal_offset)
-            second_diagonal = np.diagonal(np.fliplr(game.board), offset=diagonal_offset)
-
             for player, pieces in itertools.groupby(diagonal):
                 if player == evaluated_player:
                     score += cls.LENGTH_TO_SCORE[len(list(pieces))]
+                    if score == math.inf:
+                        return score
 
+            second_diagonal = np.diagonal(np.fliplr(game.board), offset=diagonal_offset)
             for player, pieces in itertools.groupby(second_diagonal):
                 if player == evaluated_player:
                     score += cls.LENGTH_TO_SCORE[len(list(pieces))]
+                    if score == math.inf:
+                        return score
 
         return score
 
+    """
     @classmethod
     def _other_player_can_win(cls, game: Connect4Game, player: Player) -> bool:
         for column in range(Connect4Game.BOARD_SIZE[1]):
@@ -64,14 +72,38 @@ class GreedyEvaluator:
             if game_over and winner != player:
                 return True
         return False
+    """
 
     @classmethod
     def evaluate(cls, game: Connect4Game, player: Player) -> float:
-        game_over, winner = game.check_win()
-        if game_over:
-            return math.inf if winner == player else -math.inf
+        rows_score = cls._evaluate_rows(game, player)
+        if rows_score == math.inf:
+            return math.inf
 
-        if game.current_player != player and cls._other_player_can_win(game, player):
+        columns_score = cls._evaluate_rows(game, player)
+        if columns_score == math.inf:
+            return math.inf
+
+        diagonals_score = cls._evaluate_diagonals(game, player)
+        if diagonals_score == math.inf:
+            return math.inf
+
+        opponent = Player.SECOND if player == Player.FIRST else Player.FIRST
+
+        opponent_rows_score = cls._evaluate_rows(game, opponent)
+        if opponent_rows_score == math.inf:
             return -math.inf
 
-        return cls._evaluate_rows(game, player) + cls._evaluate_columns(game, player) + cls._evaluate_diagonals(game, player)
+        opponent_columns_score = cls._evaluate_rows(game, opponent)
+        if opponent_columns_score == math.inf:
+            return -math.inf
+
+        opponent_diagonals_score = cls._evaluate_diagonals(game, opponent)
+        if opponent_diagonals_score == math.inf:
+            return -math.inf
+
+        player_score = rows_score + columns_score + diagonals_score
+        opponent_score = opponent_rows_score + opponent_columns_score + opponent_diagonals_score
+        total_score = player_score - opponent_score
+
+        return total_score
